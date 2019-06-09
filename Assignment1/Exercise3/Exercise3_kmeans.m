65a4238843090f57b2d3d72936ffcc365e2b7f8b
atussa <atussak@stud.ntnu.no>
2019-06-09 18:09:17 +0200

    Make code function

--- Assignment1/Exercise3/Exercise3_kmeans.m
+++ Assignment1/Exercise3/Exercise3_kmeans.m
@@ -1,9 +1,11 @@
-load('gesture_dataset.mat');
+function Exercise3_kmeans(gesture_l, ...
+    gesture_o, gesture_x, init_cluster_l, init_cluster_o, ...,
+    init_cluster_x, k)
+
 titles = ["l-gesture", "o-gesture", "x-gesture"];
+plots = [];
 
 %% (1) Initialization
-k = 7;
-
 gestures = {gesture_l, gesture_o, gesture_x};
 init_clusters = {init_cluster_l, init_cluster_o, init_cluster_x};
 clusters = init_clusters;
