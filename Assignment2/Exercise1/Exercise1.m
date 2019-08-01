0f5ea05acf5672fd882f1d8afebf73e07191fc38
atussa <atussak@stud.ntnu.no>
2019-08-01 20:47:07 +0200

    M-step and evaluate

--- Assignment2/Exercise1/Exercise1.m
+++ Assignment2/Exercise1/Exercise1.m
@@ -31,10 +31,28 @@ gamma = zeros(n,k);
 for i = 1:4
     gamma(:,i) = pi(i)*mvnpdf(Data', mu(i), sigma(:,:,i));
 end
+for j = 1:n
+    gamma(j,:) = gamma(j,:)/sum(gamma(j,:));
+end
 
+% M-step
 
+for i = 1:k
+    nk = sum(gamma(:,i));
+    
+    pi(i) = nk/n;
+    mu(:,i) = gamma(:,i)'*Data'/nk;
+    
+    diff = Data - mu(:,i);
+    sigma(:,:,i) = diff*diag(gamma(:,i))*diff'/nk;
+end
 
+% Evaluate
 
+loss = zeros(1,k);
+for i = 1:k
+    loss(i) = gamma(:,i)'*(log(pi(i))+log(mvnpdf(Data', mu(i), sigma(:,:,i)))-log(gamma(:,i)));
+end
 
 
 
